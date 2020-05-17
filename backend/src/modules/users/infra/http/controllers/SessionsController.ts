d8b5f49d4610339ea2763f8f01360f40256a44c7
lpbborges <luizpaulobborges@gmail.com>
2020-05-17 17:38:10 -0300

    User Profile

--- backend/src/modules/users/infra/http/controllers/SessionsController.ts
+++ backend/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-import AuthenticatUserService from '@modules/users/services/AuthenticateUserService';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
-    const authenticateUserService = container.resolve(AuthenticatUserService);
+    const authenticateUserService = container.resolve(AuthenticateUserService);
 
     const { user, token } = await authenticateUserService.execute({
       email,
--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import uplodConfig from '@config/upload';
+import uploadConfig from '@config/upload';
 
 import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
@@ -9,7 +9,7 @@ import UserAvatarController from '../controllers/UserAvatarController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uplodConfig);
+const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
